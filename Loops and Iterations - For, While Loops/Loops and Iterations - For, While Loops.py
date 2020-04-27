48ba7026c85602949b5d8bd186aa328d1fd3c150
frgtnn <rexxdragon11@gmail.com>
2020-04-27 19:43:31 +0200

    Next steps in Python

---
+++ Loops and Iterations - For, While Loops/Loops and Iterations - For, While Loops.py
@@ -0,0 +1,32 @@
+# for num in nums:
+#     print(num) shows us all of the numbers in our list
+#break przerywa np. poszukiwanie liczby (szukamy trojki, 'Found', end)
+#continue, kontynuuje loop
+# for num in nums:
+#     if num == 3:
+#         print('Found!')
+#         continue
+#     print(num)
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter) gives us list 1, 2, 3, 4, 5 do kazdego dostajemy odp 1 a, 1 b, 1c etc.
+# for i in range(1, 11):
+#     print(i) prints us ten values from one to ten
+# x = 0
+#
+# while x < 10:
+#     print(x)
+#     x += 1 dostajemy loopa az do momentu kiedy bedzie <= 10 badz >10
+# while True:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1 dostajemy loopa, ktory trwa az do 4, nie pokazuje juz wartosci 5, poniewaz zostaje przerwana (break)
+
+x = 0
+
+while True:
+    if x == 5:
+        break
+    print(x)
+    x += 1
