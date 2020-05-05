febd200de06a60e3ee401bec9a9527c864549e44
frgtnn <rexxdragon11@gmail.com>
2020-05-05 21:06:09 +0200

    Next steps in Python

---
+++ Import Modules an Exploring the Standard Library/Modules and Library.py
@@ -0,0 +1,39 @@
+
+# import sys
+# sys.path.append(':////')
+# from my_module import find_index, test
+# index = find_index(courses, 'Math')
+# # print(index)
+# # print(test)
+#
+# print(sys.path)
+# index = my_module.find_index = mm.find_index
+# import my_module = from my_moudle import find_index, the best way, we can see from where we imported all sort of things
+#from my_module import * - it says that we want to import everything from my_module
+
+# from my_module import find_index, test
+# import sys - a way to check from where our imported file is coming from
+# courses = ['History', 'Math', 'Physics', 'CompSci']
+# index = find_index(courses, 'Math')
+# print(sys.path)
+
+# import random
+# courses = ['History', 'Math', 'Physics', 'CompSci']
+# random_course = random.choice(courses)
+# print(random_course)
+
+# import math
+# courses = ['History', 'Math', 'Physics', 'CompSci']
+# rads = math.radians(90)
+# print(math.sin(rads))
+
+# import datetime
+# import calendar
+# today = datetime.date.today()
+# print(today)
+# print(calendar.isleap(2020))
+
+# import os
+# print(os.__file__)
+# print(os.getcwd()) - curernt location
+
