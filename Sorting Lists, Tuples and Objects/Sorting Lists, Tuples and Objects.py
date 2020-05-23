ede88fb9c0c4102a7ea5fe0b4247f93c9d63f82a
frgtnn <rexxdragon11@gmail.com>
2020-05-23 17:55:16 +0200

    Next steps

---
+++ Sorting Lists, Tuples and Objects/Sorting Lists, Tuples and Objects.py
@@ -0,0 +1,18 @@
+# li = [9,1,8,2,7,3,6,4,5]
+# s_li = sorted(li, reverse=True), reverse = from the end
+# print('Sorted Variable:\t', s_li)
+# li.sort()
+# print('Original Variable:\t', li)
+
+# tup = (9,1,8,2,7,3,6,4,5)
+# s_tup = sorted(tup)
+# print('Tuple\t', s_tup)
+
+# di = {'name': 'Corey', 'job': 'programming', 'age': None, 'os': 'Mac'}      #it sorts keys in the list (name, job, age, os)
+# s_di = sorted(di)
+# print('Dict\t', s_di)
+
+# li = [-6,-5,-4,1,2,3]
+# s_li = sorted(li, key=abs)       #key=abs it gives us possibility to make a good sorted list when u have negative values
+# print (s_li)
+
